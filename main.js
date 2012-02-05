1a81eaed7b8c3ed50b6801e05685255c4750eb27
Alex Jordan <alexander3223098@gmail.com>
2012-02-04 17:51:16 -0800

    Add footer, start JavaScript work, CSS improvements

---
+++ main.js
@@ -0,0 +1,20 @@
+function startup()
+{
+	document.getElementById('sentence2alternateinput').value = 'New alternate...';
+	document.getElementbyId('sentence3alternateinput').value = 'New alternate...';
+	document.getElementbyId('sentence4alternateinput').value = 'New alternate...';
+}
+
+function getAlternate(sentenceNumber)
+{
+	var alternateText;
+	if (sentenceNumber == 2) {
+		
+	}
+	if (sentenceNumber == 3) {
+		
+	}
+	if (sentenceNumber == 4) {
+		
+	}
+}
