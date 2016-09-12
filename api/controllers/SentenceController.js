8b1ec92ec5329d30a715da4f4dd918e512c56dd6
Alex Jordan <alex@strugee.net>
2016-09-12 16:32:07 -0700

    Add the sentences API

---
+++ api/controllers/SentenceController.js
@@ -0,0 +1,11 @@
+/**
+ * SentenceController
+ *
+ * @description :: Server-side logic for managing sentences
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
